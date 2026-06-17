3df488e7293b788a99191937200460b6500f6798
Heleno Sales Mesquita <heleno.sales.mesquita@gmail.com>
2026-06-17 10:37:07 -0300

    fix: usando defineConfig nativo do vinxi

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from '@tanstack/router-plugin/vite';
+import { defineConfig } from 'vinxi';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  vite: {
-    plugins: [
-      tsconfigPaths(),
-    ],
-  },
+  routers: [
+    {
+      name: 'public',
+      type: 'static',
+      dir: './public',
+      base: '/',
+    },
+    {
+      name: 'client',
+      type: 'spa',
+      handler: './src/routes/index.tsx', // Ponto de entrada das suas rotas
+      target: 'browser',
+      plugins: () => [tsconfigPaths()],
+    },
+  ],
 });
