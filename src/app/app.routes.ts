c3eca8e0cbaae223e05652b1ebc7c69177094489
Heleno Sales Mesquita <heleno.sales.mesquita@gmail.com>
2026-06-17 17:31:50 -0300

    Corrigir rotas angular com hash

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent) },
+  { path: 'home', component: HomeComponent },
   { path: '**', redirectTo: '/home' }
 ];
