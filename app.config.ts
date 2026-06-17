6321aa7eb869d74e1478d1439c593dbcf233ea74
Heleno Sales Mesquita <heleno.sales.mesquita@gmail.com>
2026-06-17 11:02:20 -0300

    feat: adicionando index.html base para renderizacao spa

--- app.config.ts
+++ app.config.ts
@@ -5,9 +5,8 @@ export default createApp({
   routers: [
     {
       name: 'client',
-      type: 'client', // Mudamos de 'spa' para 'client' para ele não exigir o index.html na raiz
-      handler: './src/routes/index.tsx', // Ponto de entrada real do seu app Lovable
-      target: 'browser',
+      type: 'spa',
+      handler: './index.html', // Agora aponta para o HTML que criamos
       plugins: () => [tsconfigPaths()],
     },
   ],
