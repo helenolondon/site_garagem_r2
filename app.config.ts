22aa1f32309c00ff60d768d4b76571424d253f5a
Heleno Sales Mesquita <heleno.sales.mesquita@gmail.com>
2026-06-17 12:03:04 -0300

    fix: alterando arquitetura do vinxi para SPA estatico

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,18 @@
-import { createApp } from 'vinxi';
+import { defineConfig } from 'vinxi';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default createApp({
+export default defineConfig({
   routers: [
     {
+      name: 'public',
+      type: 'static',
+      dir: './public',
+      base: '/',
+    },
+    {
       name: 'client',
-      type: 'client', // Mudamos para 'client' para ele não injetar proxies de SPA
-      handler: './src/router.tsx', // Aponta direto para o seu roteador principal
+      type: 'spa',
+      handler: './index.html', // Vincula o HTML base que criamos
       target: 'browser',
       plugins: () => [tsconfigPaths()],
     },
