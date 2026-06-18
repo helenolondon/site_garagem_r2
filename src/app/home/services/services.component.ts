1a222446c6c4815360b00ccd31e5bbca55e6360c
Heleno Sales Mesquita <heleno.sales.mesquita@gmail.com>
2026-06-18 12:16:24 -0300

    Adiciona serviços

--- src/app/home/services/services.component.ts
+++ src/app/home/services/services.component.ts
@@ -11,22 +11,46 @@ import { CommonModule } from '@angular/common';
 export class ServicesComponent {
   services = [
     {
-      icon: '💧',
-      title: 'Lavagem Detalhada',
-      desc: 'Lavagem técnica com shampoo neutro, descontaminação e secagem com microfibra premium.',
-      img: 'assets/hero-car.jpg'
+      icon: '✨',
+      title: 'Polimento',
+      desc: 'Correção de riscos e pequenos danos na pintura. Devolve o brilho da pintura.',
+      img: 'assets/service-polish.jpg'
     },
     {
       icon: '✨',
-      title: 'Micro pintura e pequenos reparos',
-      desc: 'Correção de riscos e pequenos danos na pintura. Devolve o aspecto de novo da pintura.',
+      title: 'Micro pintura',
+      desc: 'Correção parcial de riscos que atravessaram o verniz. Devolve o aspecto de novo do veículo.',
       img: 'assets/service-micro-pintura.png'
+    },
+    {
+      icon: '✨',
+      title: 'Pequenos reparos',
+      desc: 'Correção de pequenos danos que acontecem no dia a dia.',
+      img: 'assets/service-pequenos-retoques.png'
     },
     {
       icon: '🚗',
       title: 'Higienização Interna',
       desc: 'Limpeza profunda de bancos, carpetes, teto e plásticos. Eliminação de odores.',
       img: 'assets/service-interior.jpg'
+    },
+    {
+      icon: '🚗',
+      title: 'Restauração de peças internas',
+      desc: 'Restauração de peças internas como painel, portas, etc.',
+      img: 'assets/service-pintura-interna.png'
+    },
+    {
+      icon: '🚗',
+      title: 'Restauração de farois',
+      desc: 'Restauração completa de farois',
+      img: 'assets/service-restauracao-farol.png'
+    },
+    {
+      icon: '💧',
+      title: 'Lavagem Detalhada',
+      desc: 'Lavagem técnica com shampoo neutro, descontaminação e secagem com microfibra premium.',
+      img: 'assets/hero-car.jpg'
     },
     {
       icon: '🛡️',
