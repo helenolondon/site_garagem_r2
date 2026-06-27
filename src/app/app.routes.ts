b7ccfc23ae0b2a49a08695b0ccb6094efdb0bdf8
Heleno Sales Mesquita <heleno.sales.mesquita@gmail.com>
2026-06-27 18:44:08 -0300

    Ajustar página inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,6 @@ import { PromocoesComponent } from './promocoes/promocoes.component';
 export const routes: Routes = [
   { path: '', redirectTo: '/promocoes', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'promocoes', component: PromocoesComponent },
-  { path: '**', redirectTo: '/promocoes' }
+  // { path: 'promocoes', component: PromocoesComponent },
+  { path: '**', redirectTo: '/home' }
 ];
